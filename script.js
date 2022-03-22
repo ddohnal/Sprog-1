cd549b243cce8d3a866370d4bf402373de87b101
David Dohnal <dohnal@gjs.cz>
2022-03-22 20:59:44 +0100

    Vars, cond, loops

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+function qsa(sel){
+
+  return Array.apply(null, document.querySelectorAll(sel));
+}
+qsa(".code").forEach(function (editorEl) {
+    CodeMirror.fromTextArea(editorEl, {
+      mode: "text/x-csharp",
+      theme: "duotone-light",
+      lineNumbers: true,
+      readOnly: true,
+    });
+  });
